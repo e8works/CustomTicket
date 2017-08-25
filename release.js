997d45a2b2b9e2937eef32031aa96d10e7a47b94
amespineira <amespineira@me.com>
2017-08-25 15:38:53 -0600

    configuring

--- release.js
+++ release.js
@@ -6,11 +6,11 @@
 
 const options = {};
 
-options.ip = '127.0.0.1';
+// options.ip = '127.0.0.1';
 // options.port = parseInt(process.argv[2]);
 options.port = 8000;
-
-options.config = { name: 'Total.js' };
+// 
+// options.config = { name: 'Total.js' };
 // options.sleep = 3000;
 
 require('total.js').http('release', options);
