94f3e647f96f82fa55eea45cdeed7b1520cc0066
amespineira <amespineira@me.com>
2017-08-25 15:55:10 -0600

    debugging

--- models/login.js
+++ models/login.js
@@ -6,7 +6,8 @@ NEWSCHEMA('Login').make(function(schema) {
 	schema.addWorkflow('exec', function(error, model, controller, callback) {
 
 		var sql = DB(error);
-
+		console.log(sql)
+		console.log('***********************');
 		sql.select('item', 'tbl_user').make(function(builder) {
 			builder.fields('id', 'isactivated', 'isconfirmed');
 			builder.where('email', model.email);
@@ -14,12 +15,13 @@ NEWSCHEMA('Login').make(function(schema) {
 			builder.where('isremoved', false);
 			builder.first();
 		});
-
 		sql.validate('item', 'error-user-credentials');
 
 		sql.exec(function(err, response) {
-			if (err)
+			if (err) {
+				console.log('error ran');
 				return callback();
+			}
 			!response.isactivated && error.push('error-user-activated');
 			!response.isconfirmed && error.push('error-user-confirmed');
 			response.isconfirmed && response.isactivated && controller.cookie(CONFIG('auth.cookie'), F.encrypt({ id: response.id, date: F.datetime.getTime(), ip: controller.ip }, CONFIG('auth.secret')), '1 month');
